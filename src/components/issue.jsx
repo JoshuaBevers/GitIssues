971b1f030d0d8c651a2b1def2be729b9e222f591
infernezor@gmail.com <infernezor@gmail.com>
2020-03-31 09:58:23 -0400

    prep for branch change.

--- src/components/issue.jsx
+++ src/components/issue.jsx
@@ -2,11 +2,10 @@ import React from "react";
 
 const Issue = props => {
   const { gitIssue } = props;
-  console.log("the url is", gitIssue);
   if (gitIssue !== undefined) {
     return (
       <div>
-        the issue: "{gitIssue.title}" is:{" "}
+        the issue: "{gitIssue.title}" is:
         <a href={gitIssue.url}>{gitIssue.url}</a>
         <p>{gitIssue.body}</p>
       </div>
